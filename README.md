7537c769e06c4efe31203995e0aa6cd2a1105ae3
snoweek <1052132917@qq.com>
2016-05-06 20:10:39 +0800

    完善数据库

--- README.md
+++ README.md
@@ -13,8 +13,8 @@ CREATE TABLE `manager` (
   `manager_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `name` text,
   `password` text,
-  PRIMARY KEY (`id`)
-) 
+  PRIMARY KEY (`manager_id`)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8; 
 ```
 表manager包含三列，manager_id记录管理员的数量，同时用作主键;name记录管理员的姓名;password记录管理员的密码。
 
@@ -25,8 +25,8 @@ CREATE TABLE `student` (
   `name` text,
   `gender` int(11) DEFAULT NULL,
   `major_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-)
+  PRIMARY KEY (`student_id`)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8; 
 ```
 表student包含四列，student_id用作主键;name记录学生姓名;gender记录学生的性别;major_id指示学生所学的专业。
 
@@ -37,8 +37,8 @@ CREATE TABLE `student` (
   `major_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `name` text,
   `college` text,
-  PRIMARY KEY (`id`)
-) 
+  PRIMARY KEY (`major_id`)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;  
 ```
 表manager包含三列，major_id用作主键;name记录专业名称;content记录专业所在的院系名称。
 
---
+++ student_major.sql
@@ -0,0 +1,104 @@
+-- MySQL dump 10.13  Distrib 5.5.49, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: student_major
+-- ------------------------------------------------------
+-- Server version	5.5.49-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `major`
+--
+
+DROP TABLE IF EXISTS `major`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `major` (
+  `major_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `college` text,
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`major_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `major`
+--
+
+LOCK TABLES `major` WRITE;
+/*!40000 ALTER TABLE `major` DISABLE KEYS */;
+/*!40000 ALTER TABLE `major` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manager`
+--
+
+DROP TABLE IF EXISTS `manager`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `manager` (
+  `manager_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `password` text,
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`manager_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manager`
+--
+
+LOCK TABLES `manager` WRITE;
+/*!40000 ALTER TABLE `manager` DISABLE KEYS */;
+INSERT INTO `manager` VALUES (1,'孙岩','123456',0);
+/*!40000 ALTER TABLE `manager` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `student` (
+  `student_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `gender` int(11) DEFAULT NULL,
+  `major_id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-05-06 20:06:23
